cf8548bc653202e17cd7f81ee2c1819b57b2d75e
ofargally <ofargally@colgate.edu>
2024-11-26 23:44:31 +0200

    added get all user routes, updated post users to only accept non-preexisting emails

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,3 +1,4 @@
+from typing import List
 from .. import utils, models
 from ..schemas import UserCreate, UserResponse
 from ..database import get_db
@@ -16,6 +17,12 @@ router = APIRouter(
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=UserResponse)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     print("user created")
+    # Check if user has already been created
+    check_user_query = db.query(models.User).filter(
+        models.User.email == user.email)
+    if check_user_query.first():
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail="User with this email already exists")
     # Hash password and apply
     user.password = utils.hash(user.password)
     new_user = models.User(**user.model_dump())
@@ -25,9 +32,14 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 
+@router.get("/", response_model=List[UserResponse])
+def get_all_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
+
+
 @router.get("/{id}", response_model=UserResponse)
 def get_user(id: int, db: Session = Depends(get_db)):
-    print("lllll")
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
         raise (HTTPException(status_code=status.HTTP_404_NOT_FOUND,
